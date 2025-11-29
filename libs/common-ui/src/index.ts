0e8c2a0f6ceea3ff8f0cc80773ecfc51e8436115
Кирилл Захаров <boone24@yandex.ru>
2025-11-29 09:42:57 +0700

    Фиксы по первому ДЗ

--- libs/common-ui/src/index.ts
+++ libs/common-ui/src/index.ts
@@ -1,5 +1,5 @@
 import {Dnd} from "./lib/directives/dnd"
-import { ScrollDownDirective } from "./lib/directives/scroll-down"
+import {ScrollDownDirective} from "./lib/directives/scroll-down"
 import {InfiniteScrollTrigger} from "./lib/infinite-scroll-trigger/infinite-scroll-trigger"
 import {ImgUrlPipe} from "./lib/pipes/img-url-pipe"
 import {TimeAgoPipe} from "./lib/pipes/time-ago-pipe"
