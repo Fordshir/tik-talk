c706decef74fdab8a74cce4c8f57c43b7c7c511a
Кирилл Захаров <boone24@yandex.ru>
2025-11-29 09:28:12 +0700

    Фиксы по первому ДЗ

--- libs/common-ui/src/lib/components/select/select.ts
+++ libs/common-ui/src/lib/components/select/select.ts
@@ -42,6 +42,8 @@ export class Select implements ControlValueAccessor {
 
   onClick (event: Event) {
     const target = event.target as HTMLElement;
+    event.stopPropagation();
+    event.preventDefault();
 
     if (!target.closest('button')){
       this.toggleDropdown()
